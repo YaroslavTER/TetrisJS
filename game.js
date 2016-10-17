7ae6f240ca804bdaa5d87b5addc2eaf735d3adf5
Yaroslav <yaroslavtereshchuk@gmail.com>
2016-10-17 22:33:50 +0300

    colorchanger

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ var canvas = document.getElementById('canvas_id')
 var ctx = canvas.getContext('2d')
 var side = 20, rows = 10, colums = 20, time = 500, falltime = time
 var figures = [], colors = ['#07977B', '#424C4A','#7E1B28','#207E1B']
-var current_color
+var current_color = ''
 
 var figureTemplates = [
     [[0,0],[0,1],[0,2],[1,0]],
@@ -13,8 +13,13 @@ var figureTemplates = [
 
 function AddFigure(){
     var f = { x: 0, y: 0}
+    var new_color = colors[getRandInt(0,colors.length)]
+    while(new_color == current_color){
+        new_color = colors[getRandInt(0,colors.length)]
+    }
+    f.color = new_color
+    current_color = new_color
     f.blocks = figureTemplates[getRandInt(0,3)]
-    f.color = colors[getRandInt(0,colors.length)]
     figures.push(f)
 }
 
